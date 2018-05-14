b99f6e7662c08de758dfc997b42b7d33d1ee47ed
Alexander Armua <alexanderarmua1@gmail.com>
2018-05-14 00:33:12 -0300

    Terminada la parte principal, faltan los puntos 3 al ultimo

--- main.hs
+++ main.hs
@@ -26,16 +26,14 @@ ordenLuca = Orden "Luca" (Receta 0 1 1 2 3) 5 False
 
 data Cliente = Cliente {
     nombre              :: String,
-    criterioDePaciencia :: Int
-} deriving (Show)
+    criterioDePaciencia :: [Orden] -> Int
+} 
 
---criterioDePaciencia :: [Orden] -> Int
-{-
 pepe = Cliente "Pepe" constante
 jose = Cliente "Jose" mucha
 alex = Cliente "Alex" (lineal 5)
 luca = Cliente "Luca" (lineal 1)
--}
+
 ordenesPasteleria = [ordenPepe, ordenJose, ordenAlex, ordenLuca]
 
 {-  Punto 1: Saber si la receta sale bien.
@@ -61,6 +59,48 @@ ingredientesProporcionales receta = even (cucharadasDeAzucar receta) && even (cu
 saleBien :: Receta -> Bool
 saleBien receta = recetaNoAceitosa receta && cantidadJustaHuevos receta && ingredientesProporcionales receta
 
+{- Punto 2: 
+    a) Crear los criterios de paciencia. 
+        constante: Espera hasta 8 horas.
+        lineal: Cantidad de horas propia mas 1 hora por torta.
+        mucha: <=2 tortas => 2 horas; else; 3 horas por torta.
+    b) Dado un cliente saber cuanto puede tardar como maximo
+-}
+
+-- Punto a
+
+constante :: [Orden] -> Int
+constante ordenes = 8
+
+-- Espera + LA SUMA del mapeo de ordenes a CANTIDAD
+-- Transforma un array de ordenes en array de cantidades de tortas y luego los suma todos
+lineal :: Int -> [Orden] -> Int
+lineal esperaBase ordenes = esperaBase + sum (map cantidad ordenes)
+
+mucha :: [Orden] -> Int
+mucha [_] = 2
+mucha [_,_] = 2
+mucha ordenes = 3 * sum (map cantidad ordenes)
+
+-- Punto b
+
+-- Ademas, necesito saber cuales son las ordenes de un cliente
+ordenesCliente :: Cliente -> [Orden] -> [Orden]
+ordenesCliente cliente = filter ((nombre cliente ==).nombreCliente)
+
+    -- ordenesClienteDeprecated :: Cliente -> [Orden] -> [Orden]
+    -- ordenesClienteDeprecated cliente ordenes = filter ((nombre cliente ==).nombreCliente) ordenes
+
+cuantoPuedoTardar :: Cliente -> Int
+cuantoPuedoTardar cliente = criterioDePaciencia cliente (ordenesCliente cliente ordenesPasteleria)
+
+
+{- Punto 3:
+    a) Dado un ingrediente decir cuanto se necesita
+    b) Saber si la pasteleria tiene poco trabajo
+    c) Saber cuanto tarda una orden en la pasteleria
+    d) Desarrollar la funcion clienteSatisfecho
+-}
 
 
 
